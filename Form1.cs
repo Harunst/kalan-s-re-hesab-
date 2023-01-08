013c7c5296a13a8a87ae2820365acb9075911549
harun <harun@HARUN-TURUNC>
2023-01-08 23:21:59 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kalan_süre_hesabı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string girisZamani = DateTime.Now.ToLongTimeString();
+            string cikisZamani = textBox1.Text;
+            TimeSpan girisCikisFarki = DateTime.Parse(cikisZamani).Subtract(DateTime.Parse(girisZamani));
+            string calismaSuresi = girisCikisFarki.ToString();
+            lblKalanSure.Text = calismaSuresi.ToString();
+        }
+    }
+}
